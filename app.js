068b84b840d619f8150ff8903a39fac061f89be8
caizongding <caizongding@gmail.com>
2019-11-09 09:38:24 +0800

    add nodemon deps

--- app.js
+++ app.js
@@ -1,39 +1,36 @@
-const Koa = require('koa');
+const Koa = require("koa");
 const app = new Koa();
 
 // logger
 
-app.use(async (ctx, next) => {
-  console.log('before logger');
-  
-  await next();
-  console.log('after logger');
+// app.use(async (ctx, next) => {
+//   console.log('before logger');
 
-  const rt = ctx.response.get('X-Response-Time');
-  console.log(`${ctx.method} ${ctx.url} - ${rt}`);
-});
+//   await next();
+//   console.log('after logger');
+// });
 
 // x-response-time
 
 app.use(async (ctx, next) => {
   const start = Date.now();
-  console.log('before xxx');
+  //console.log("before xxx");
 
   await next();
-  console.log('after xxx');
+  //console.log("after xxx");
 
   const ms = Date.now() - start;
-  ctx.set('X-Response-Time', `${ms}ms`);
+  ctx.set("X-Response-Time", `${ms}ms`);
 });
 
 // response
 
 app.use(async ctx => {
-  console.log('response');
+  console.log(`${ctx.method} ${ctx.url}`);
   ctx.body = {
     message: "Page Not Found"
-  }
+  };
   // ctx.body = 'Hello World';
 });
 
-app.listen(3000);
+app.listen(3001);
